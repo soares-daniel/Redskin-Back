048f999faa6dc7a833301dc8cebd6b1a07a1b572
soares-daniel <daniel.soares.001@student.uni.lu>
2023-05-29 18:29:30 +0200

    changed deprecated crypt to hashlib alternative

--- app/security/hashing/hash.py
+++ app/security/hashing/hash.py
@@ -1,41 +1,42 @@
-from passlib.context import CryptContext
-
+import hashlib
+import binascii
 from app.config.manager import settings
 
 
 class HashGenerator:
     def __init__(self):
-        self._hash_ctx_layer_1: CryptContext = CryptContext(
-            schemes=[settings.HASHING_ALGORITHM_LAYER_1], deprecated="auto"
-        )
-        self._hash_ctx_layer_2: CryptContext = CryptContext(
-            schemes=[settings.HASHING_ALGORITHM_LAYER_2], deprecated="auto"
-        )
         self._hash_ctx_salt: str = settings.HASHING_SALT
 
     @property
     def _get_hashing_salt(self) -> str:
         return self._hash_ctx_salt
 
-    @property
-    def generate_password_salt_hash(self) -> str:
+    def generate_password_salt_hash(self, password: str) -> str:
         """
-        A function to generate a hash from Bcrypt to append to the user password.
+        A function to generate a hash from SHA256 to append to the user password.
         """
-        return self._hash_ctx_layer_1.hash(secret=self._get_hashing_salt)
+        if not self._get_hashing_salt:
+            raise ValueError("Invalid salt value.")
+        dk = hashlib.pbkdf2_hmac('sha256', password.encode(), self._get_hashing_salt.encode(), 100000)
+        return binascii.hexlify(dk).decode()
 
     def generate_password_hash(self, hash_salt: str, password: str) -> str:
         """
-        A function that adds the user's password with the layer 1 Bcrypt hash, before
-        hash it for the second time using Argon2 algorithm.
+        A function that adds the user's password with the layer 1 SHA256 hash, before
+        hash it for the second time using SHA512 algorithm.
         """
-        return self._hash_ctx_layer_2.hash(secret=hash_salt + password)
+        if not hash_salt or not password:
+            raise ValueError("Invalid salt or password value.")
+        dk = hashlib.pbkdf2_hmac('sha512', (hash_salt + password).encode(), self._get_hashing_salt.encode(), 100000)
+        return binascii.hexlify(dk).decode()
 
     def is_password_verified(self, password: str, hashed_password: str) -> bool:
         """
-        A function that decodes users' password and verifies whether it is the correct password.
+        A function that verifies whether the password matches the hashed password.
         """
-        return self._hash_ctx_layer_2.verify(secret=password, hash=hashed_password)
+        if not password or not hashed_password:
+            raise ValueError("Invalid password or hash.")
+        return self.generate_password_hash(self.generate_password_salt_hash(password), password) == hashed_password
 
 
 def get_hash_generator() -> HashGenerator:
