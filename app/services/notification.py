d253c1209e29c34e4c6896b1610e60eb3df1001a
soares-daniel <daniel.soares.001@student.uni.lu>
2023-07-13 21:19:28 +0200

    refactoring, handle when discord server not connected

--- app/services/notification.py
+++ app/services/notification.py
@@ -1,4 +1,5 @@
 import aiohttp
+from aiohttp import ServerDisconnectedError
 
 from app.models.schemas.event_type import EventTypeInResponse
 from app.models.schemas.role import RoleInResponse
@@ -13,73 +14,80 @@ from app.models.schemas.event_operation import EventOperation
 
 class NotificationService(BaseService):
     @staticmethod
-    async def send_event_notification(event: EventInResponse, event_operation: EventOperation) -> None:
-        async with aiohttp.ClientSession() as session:
-            payload = {
-                "event_operation": event_operation.value,
-                "event": event.json()
-            }
-            async with session.post(settings.DISCORD_URL, json=payload) as resp:
-                print(resp.status)
-                print(await resp.text())
+    async def send_notification(
+            url: str,
+            payload: dict
+    ) -> None:
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(url, json=payload) as resp:
+                    print(resp.status)
+                    print(await resp.text())
+        except ServerDisconnectedError:
+            print("Server disconnected")
 
-    @staticmethod
-    async def send_event_type_notification(event_type: EventTypeInResponse, event_operation: EventOperation) -> None:
-        async with aiohttp.ClientSession() as session:
-            payload = {
-                "event_operation": event_operation.value,
-                "event": event_type.json()
-            }
-            async with session.post(settings.DISCORD_URL, json=payload) as resp:
-                print(resp.status)
-                print(await resp.text())
+    async def send_event_notification(
+            self,
+            event: EventInResponse,
+            event_operation: EventOperation
+    ) -> None:
+        payload = {
+            "event_operation": event_operation.value,
+            "event": event.json()
+        }
+        await self.send_notification(settings.DISCORD_URL, payload)
 
-    @staticmethod
-    async def send_user_notification(user: UserInResponse, event_operation: EventOperation) -> None:
-        async with aiohttp.ClientSession() as session:
-            payload = {
-                "event_operation": event_operation.value,
-                "event": user.json()
-            }
-            async with session.post(settings.DISCORD_URL, json=payload) as resp:
-                print(resp.status)
-                print(await resp.text())
+    async def send_event_type_notification(
+            self,
+            event_type: EventTypeInResponse,
+            event_operation: EventOperation
+    ) -> None:
+        payload = {
+            "event_operation": event_operation.value,
+            "event": event_type.json()
+        }
+        await self.send_notification(settings.DISCORD_URL, payload)
 
-    @staticmethod
-    async def send_role_notification(role: RoleInResponse, event_operation: EventOperation) -> None:
-        async with aiohttp.ClientSession() as session:
-            payload = {
-                "event_operation": event_operation.value,
-                "event": role.json()
-            }
-            async with session.post(settings.DISCORD_URL, json=payload) as resp:
-                print(resp.status)
-                print(await resp.text())
+    async def send_user_notification(
+            self,
+            user: UserInResponse,
+            event_operation: EventOperation
+    ) -> None:
+        payload = {
+            "event_operation": event_operation.value,
+            "event": user.json()
+        }
+        await self.send_notification(settings.DISCORD_URL, payload)
 
-    @staticmethod
+    async def send_role_notification(
+            self,
+            role: RoleInResponse,
+            event_operation: EventOperation
+    ) -> None:
+        payload = {
+            "event_operation": event_operation.value,
+            "event": role.json()
+        }
+        await self.send_notification(settings.DISCORD_URL, payload)
+
     async def send_user_role_notification(
+            self,
             user_role: UserRoleInAssign | UserRoleInRemove,
             event_operation: EventOperation
     ) -> None:
-        async with aiohttp.ClientSession() as session:
-            payload = {
-                "event_operation": event_operation.value,
-                "event": user_role.json()
-            }
-            async with session.post(settings.DISCORD_URL, json=payload) as resp:
-                print(resp.status)
-                print(await resp.text())
+        payload = {
+            "event_operation": event_operation.value,
+            "event": user_role.json()
+        }
+        await self.send_notification(settings.DISCORD_URL, payload)
 
-    @staticmethod
     async def send_permission_notification(
+            self,
             permission: RoleEventTypeInResponse,
             event_operation: EventOperation
     ) -> None:
-        async with aiohttp.ClientSession() as session:
-            payload = {
-                "event_operation": event_operation.value,
-                "event": permission.json()
-            }
-            async with session.post(settings.DISCORD_URL, json=payload) as resp:
-                print(resp.status)
-                print(await resp.text())
+        payload = {
+            "event_operation": event_operation.value,
+            "event": permission.json()
+        }
+        await self.send_notification(settings.DISCORD_URL, payload)
