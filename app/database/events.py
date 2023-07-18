f033a7b3cd9b92abb33d8066491b897f8580197d
soares-daniel <daniel.soares.001@student.uni.lu>
2023-07-18 21:35:00 +0200

    refactoring

--- app/database/events.py
+++ app/database/events.py
@@ -1,5 +1,7 @@
 import fastapi
-import loguru
+
+from loguru import logger
+
 from sqlalchemy import event
 from sqlalchemy.dialects.postgresql.asyncpg import AsyncAdapt_asyncpg_connection
 from sqlalchemy.ext.asyncio import AsyncConnection
@@ -13,40 +15,40 @@ from app.database.table import Base
 def inspect_db_server_on_connection(
     db_api_connection: AsyncAdapt_asyncpg_connection, connection_record: _ConnectionRecord
 ) -> None:
-    loguru.logger.info(f"New DB API Connection ---\n {db_api_connection}")
-    loguru.logger.info(f"Connection Record ---\n {connection_record}")
+    logger.info(f"New DB API Connection ---\n {db_api_connection}")
+    logger.info(f"Connection Record ---\n {connection_record}")
 
 
 @event.listens_for(target=async_db.async_engine.sync_engine, identifier="close")
 def inspect_db_server_on_close(
     db_api_connection: AsyncAdapt_asyncpg_connection, connection_record: _ConnectionRecord
 ) -> None:
-    loguru.logger.info(f"Closing DB API Connection ---\n {db_api_connection}")
-    loguru.logger.info(f"Closed Connection Record ---\n {connection_record}")
+    logger.info(f"Closing DB API Connection ---\n {db_api_connection}")
+    logger.info(f"Closed Connection Record ---\n {connection_record}")
 
 
 async def initialize_db_tables(connection: AsyncConnection) -> None:
-    loguru.logger.info("Database Table Creation --- Initializing . . .")
+    logger.info("Database Table Creation --- Initializing . . .")
 
     await connection.run_sync(Base.metadata.create_all)
 
-    loguru.logger.info("Database Table Creation --- Successfully Initialized!")
+    logger.info("Database Table Creation --- Successfully Initialized!")
 
 
 async def init_db_connection(app: fastapi.FastAPI) -> None:
-    loguru.logger.info("Database Connection --- Establishing . . .")
+    logger.info("Database Connection --- Establishing . . .")
 
     app.state.db = async_db  # type: ignore
 
     async with app.state.db.async_engine.begin() as connection:  # type: ignore
         await initialize_db_tables(connection=connection)
 
-    loguru.logger.info("Database Connection --- Successfully Established!")
+    logger.info("Database Connection --- Successfully Established!")
 
 
 async def close_db_connection(app: fastapi.FastAPI) -> None:
-    loguru.logger.info("Database Connection --- Disposing . . .")
+    logger.info("Database Connection --- Disposing . . .")
 
     await app.state.db.async_engine.dispose()  # type: ignore
 
-    loguru.logger.info("Database Connection --- Successfully Disposed!")
+    logger.info("Database Connection --- Successfully Disposed!")
