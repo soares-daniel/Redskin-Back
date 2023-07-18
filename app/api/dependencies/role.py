a2f638db6a03d33f8927e047142c7f68f1c13fe2
soares-daniel <daniel.soares.001@student.uni.lu>
2023-07-18 22:08:49 +0200

    fixed role dependency

--- app/api/dependencies/role.py
+++ app/api/dependencies/role.py
@@ -3,6 +3,7 @@ from app.repositories.user import UserRepository
 from app.api.dependencies.authentication import get_current_user
 from app.api.dependencies.repository import get_repository
 from app.models.db.user import User
+from app.utilities.exceptions.database import EntityDoesNotExist
 from app.utilities.exceptions.http.exc_403 import http_403_exc_missing_role
 
 
@@ -11,7 +12,11 @@ def is_user_in_role(role: str):
             current_user: User = Depends(get_current_user),
             user_repo: UserRepository = Depends(get_repository(repo_type=UserRepository))
     ) -> User:
-        roles = await user_repo.get_roles_for_user(user_id=current_user.id)
+        try:
+            roles = await user_repo.get_roles_for_user(user_id=current_user.id)
+        except EntityDoesNotExist:
+            raise http_403_exc_missing_role()
+
         for entry in roles:
             if entry.name == role:
                 return current_user
