e83e42a23e05226f12553c4bcd955ecd624fb8c1
soares-daniel <daniel.soares.001@student.uni.lu>
2023-07-18 21:35:38 +0200

    added routes logging dependency to log incoming requests

---
+++ app/api/dependencies/logging.py
@@ -0,0 +1,15 @@
+import fastapi
+from loguru import logger
+
+
+routes_logger = logger.bind(name="routes")
+
+
+async def logging_dependency(request: fastapi.Request):
+    routes_logger.debug(f"{request.method} {request.url}")
+    routes_logger.debug("Params:")
+    for name, value in request.path_params.items():
+        routes_logger.debug(f"\t{name}: {value}")
+    routes_logger.debug("Headers:")
+    for name, value in request.headers.items():
+        routes_logger.debug(f"\t{name}: {value}")
--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,9 +1,11 @@
 import fastapi
 
+from app.api.dependencies.logging import logging_dependency
 from app.api.routes import user, event, authentication, role, admin
 
 routers = [user.router, event.router, authentication.router, role.router, admin.router]
 main_router = fastapi.APIRouter()
 
+# Global logging dependency, can be used in any router or endpoint individually
 for router in routers:
-    main_router.include_router(router)
+    main_router.include_router(router, dependencies=[fastapi.Depends(logging_dependency)])
